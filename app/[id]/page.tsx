a822dc1189f8887390b7a46d93f963e7652b9436
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-10-07 22:05:10 +0900

    Feat : image에 alt 추가

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -4,25 +4,28 @@ import { Invitation, Location } from "types/invitation.module";
 import { notFound } from "next/navigation";
 
 interface InvitationPageProps {
-    params: {
-        id: number
-    }
+  params: {
+    id: number;
+  };
 }
 
 export default async function InvitationPage({ params }: InvitationPageProps) {
-    const id = params.id;
+  const id = params.id;
 
-    let invitationInfo: { invitation: Invitation, location: Location };
+  let invitationInfo: { invitation: Invitation; location: Location };
 
-    try {
-        invitationInfo = await getInvitation(id);
+  try {
+    invitationInfo = await getInvitation(id);
 
-        return (
-            <div>
-                <TitleContents invitation={invitationInfo.invitation} location={invitationInfo.location} />
-            </div>
-        )
-    } catch (e) {
-        notFound();
-    }
+    return (
+      <main>
+        <TitleContents
+          invitation={invitationInfo.invitation}
+          location={invitationInfo.location}
+        />
+      </main>
+    );
+  } catch (e) {
+    notFound();
+  }
 }
--- components/title_content/TitleContents.tsx
+++ components/title_content/TitleContents.tsx
@@ -6,15 +6,23 @@ import classes from "./TitleContents.module.scss";
 import { Invitation, Location } from "types/invitation.module";
 
 interface TitleContentsPropsType {
-    invitation: Invitation;
-    location: Location;
+  invitation: Invitation;
+  location: Location;
 }
 
 /* Todo : 나중에 이미지는 서버에서 가져와야 함 */
-export default function TitleContents({ invitation, location } : TitleContentsPropsType) {
+export default function TitleContents({
+  invitation,
+  location,
+}: TitleContentsPropsType) {
   return (
     <section className={classes.content_wrapper}>
-      <Image src={mainImage} alt="" className={classes.image} priority />
+      <Image
+        src={mainImage}
+        alt="웨딩 사진"
+        className={classes.image}
+        priority
+      />
       <div className={classes.text_wrapper}>
         <p className={classes.name_wrapper}>
           <span>{invitation.bride_name}</span>
