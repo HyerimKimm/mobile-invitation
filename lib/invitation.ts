d30f9eaf6ee0391fbc4332a600315e7d1bd319e7
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-10-09 16:42:16 +0900

    Feat : sqlite 관련 로직 sqlite 하위로 옮김

--- lib/invitation.ts
+++ lib/invitation.ts
@@ -2,23 +2,27 @@ import sql from "better-sqlite3";
 import { Invitation, Location } from "types/invitation.module";
 import generateError from "./generateError";
 
-const db = sql("mobile_invitation.db");
+const db = sql("./sqlite/mobile_invitation.db");
 
-export async function getInvitation(id: number): Promise<{ invitation: Invitation, location: Location }>{
-    await new Promise((resolve)=>setTimeout(resolve, 1000));
+export async function getInvitation(
+  id: number
+): Promise<{ invitation: Invitation; location: Location }> {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    const invitationData = db.prepare(
-        `SELECT  * FROM invitations WHERE id = ? `
-    ).get(id) as Invitation;
-    
-    const locationData = db.prepare(`SELECT * FROM locations WHERE id = ?`).get(invitationData.location_id) as Location;
+  const invitationData = db
+    .prepare(`SELECT  * FROM invitations WHERE id = ? `)
+    .get(id) as Invitation;
 
-    if(invitationData && locationData) {
-        return {
-            invitation: invitationData,
-            location: locationData
-        };
-    } else {
-       generateError('잘못된 id입니다.', 404);
-    }
+  const locationData = db
+    .prepare(`SELECT * FROM locations WHERE id = ?`)
+    .get(invitationData.location_id) as Location;
+
+  if (invitationData && locationData) {
+    return {
+      invitation: invitationData,
+      location: locationData,
+    };
+  } else {
+    generateError("잘못된 id입니다.", 404);
+  }
 }
--- sqlite/initdb.ts
+++ sqlite/initdb.ts
@@ -2,39 +2,40 @@ const sql = require("better-sqlite3");
 const db = sql("mobile_invitation.db");
 
 const dummyInvitations = [
-    {
-        id: 1,
-        groom_name: '홍길동',
-        bride_name: '홍길순',
-        groom_dad_name: '홍길태',
-        groom_mom_name: '홍길자',
-        bride_dad_name: '홍길용',
-        bride_mom_name: '홍길연',
-        start_date: '2030-09-12',
-        start_time: '13:00:00',
-        location_id: 1,
-        parking_info: {
-            location: '테크노마트 지하주차장 이용 (B3 ~ B7)',
-            price: '2시간 무료주차이며, 이후 10분당 1000원이 부과됩니다.'
-        },
-        subwayInfo: {
-            station_name: '신도림역',
-            exit_number: 8,
-            avail_line: [ 1, 2 ]
-        }, 
+  {
+    id: 1,
+    groom_name: "홍길동",
+    bride_name: "홍길순",
+    groom_dad_name: "홍길태",
+    groom_mom_name: "홍길자",
+    bride_dad_name: "홍길용",
+    bride_mom_name: "홍길연",
+    start_date: "2030-09-12",
+    start_time: "13:00:00",
+    location_id: 1,
+    parking_info: {
+      location: "테크노마트 지하주차장 이용 (B3 ~ B7)",
+      price: "2시간 무료주차이며, 이후 10분당 1000원이 부과됩니다.",
     },
-]
+    subwayInfo: {
+      station_name: "신도림역",
+      exit_number: 8,
+      avail_line: [1, 2],
+    },
+  },
+];
 
 const dummyLocations = [
-    {
-        id: 1,
-        title: '엄청난 웨딩홀 2층 Marcy Hall',
-        detail: '서울 양천구 오목로1길 12',
-        tel: '02-2222-2222'
-    }
-]
+  {
+    id: 1,
+    title: "엄청난 웨딩홀 2층 Marcy Hall",
+    detail: "서울 양천구 오목로1길 12",
+    tel: "02-2222-2222",
+  },
+];
 
-db.prepare(`
+db.prepare(
+  `
     CREATE TABLE IF NOT EXISTS invitations (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         groom_name TEXT,
@@ -47,19 +48,22 @@ db.prepare(`
         start_time TEXT,
         location_id INTEGER
     )
-`).run();
+`
+).run();
 
-db.prepare(`
+db.prepare(
+  `
     CREATE TABLE IF NOT EXISTS locations (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT,
         detail TEXT,
         tel TEXT
     )
-`).run();
+`
+).run();
 
 async function initLocations() {
-    const stmt = db.prepare(`
+  const stmt = db.prepare(`
         INSERT INTO locations VALUES (
             null,
             @title,
@@ -68,13 +72,13 @@ async function initLocations() {
         )
     `);
 
-    for(const location of dummyLocations) {
-        stmt.run(location);
-    }
+  for (const location of dummyLocations) {
+    stmt.run(location);
+  }
 }
 
 async function initInvitations() {
-    const stmt = db.prepare(`
+  const stmt = db.prepare(`
         INSERT INTO invitations VALUES (
             null,
             @groom_name,
@@ -89,9 +93,9 @@ async function initInvitations() {
         )
     `);
 
-    for (const invitation of dummyInvitations) {
-        stmt.run(invitation);
-    }
+  for (const invitation of dummyInvitations) {
+    stmt.run(invitation);
+  }
 }
 
 initLocations();
