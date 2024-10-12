ba8655d65641753330ee3999820e151b5d310f51
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-10-12 13:47:22 +0900

    Feat. Firestore에서 데이터 가져옴

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
 import classes from "@app/page.module.scss";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-export default function Home() {
+const firebaseConfig = {
+  apiKey: "AIzaSyDXb2I-hFMo0HPGM4YTHAWs3lU-T28rCDQ",
+  authDomain: "mobileinvitation-5840a.firebaseapp.com",
+  projectId: "mobileinvitation-5840a",
+  storageBucket: "mobileinvitation-5840a.appspot.com",
+  messagingSenderId: "523966684809",
+  appId: "1:523966684809:web:60d7e38b36a3fb2d30f058",
+  measurementId: "G-C23L8PLRX3",
+};
+
+export default async function Home() {
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  const querySnapshot = await getDocs(collection(db, "invitations"));
+
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+  });
+
   return (
     <main className={classes.page_wrapper}>
       안녕하세요, 모바일 청첩장 프로젝트입니다.
--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -3,11 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDXb2I-hFMo0HPGM4YTHAWs3lU-T28rCDQ",
   authDomain: "mobileinvitation-5840a.firebaseapp.com",
@@ -18,9 +13,6 @@ const firebaseConfig = {
   measurementId: "G-C23L8PLRX3",
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
+export const db = getFirestore(app);
