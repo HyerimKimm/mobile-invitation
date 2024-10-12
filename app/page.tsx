75bbc846affe62ac5f51637a8a95f0e662012ffb
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-10-12 14:10:24 +0900

    Feat: Firebase Config를 환경 변수로 옮김

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,7 @@ import classes from "@app/page.module.scss";
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDXb2I-hFMo0HPGM4YTHAWs3lU-T28rCDQ",
-  authDomain: "mobileinvitation-5840a.firebaseapp.com",
-  projectId: "mobileinvitation-5840a",
-  storageBucket: "mobileinvitation-5840a.appspot.com",
-  messagingSenderId: "523966684809",
-  appId: "1:523966684809:web:60d7e38b36a3fb2d30f058",
-  measurementId: "G-C23L8PLRX3",
-};
+import { firebaseConfig } from "@firebase/firebaseConfig";
 
 export default async function Home() {
   const app = initializeApp(firebaseConfig);
