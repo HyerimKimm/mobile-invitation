f732a5cea5acd245a06be55fd43a1bfa822f543d
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-10-30 23:19:26 +0900

    Feat: docSnap.exist()로 존재 여부 검사 후 데이터 사용하기

--- firebase/invitation.ts
+++ firebase/invitation.ts
@@ -14,26 +14,50 @@ export async function getInvitation(
   const docSnap = await getDoc(q);
   const storage = getStorage(app);
 
-  const url = await getDownloadURL(
-    ref(storage, docSnap.data().background_img_url)
-  );
+  if (docSnap.exists()) {
+    const url = await getDownloadURL(
+      ref(storage, docSnap.data().background_img_url)
+    );
 
-  const invitation = {
-    id: q.id,
-    ...docSnap.data(),
-    background_img_url: url,
-  } as Invitation;
+    const invitation = {
+      id: q.id,
+      ...docSnap.data(),
+      background_img_url: url,
+    } as Invitation;
 
-  const locationQ = doc(db, "locations", invitation.location_id);
-  const locationDocSnap = await getDoc(locationQ);
+    const locationQ = doc(db, "locations", invitation.location_id);
+    const locationDocSnap = await getDoc(locationQ);
 
-  const location = {
-    id: locationQ.id,
-    ...locationDocSnap.data(),
-  } as Location;
+    const location = {
+      id: locationQ.id,
+      ...locationDocSnap.data(),
+    } as Location;
 
-  return {
-    invitation: invitation,
-    location: location,
-  };
+    return {
+      invitation: invitation,
+      location: location,
+    };
+  } else {
+    return {
+      invitation: {
+        id: 0,
+        groom_name: "",
+        bride_name: "",
+        groom_dad_name: "",
+        groom_mom_name: "",
+        bride_dad_name: "",
+        bride_mom_name: "",
+        start_date: "",
+        start_time: "",
+        location_id: "",
+        background_img_url: "",
+      },
+      location: {
+        id: 0,
+        title: "",
+        detail: "",
+        tel: "",
+      },
+    };
+  }
 }
